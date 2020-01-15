3b14d0d986873737da97625d01eb363a3fe2c5b2
Sam Rust <xsrust@gmail.com>
2020-01-15 13:42:49 +0000

    add in docker config, patch mysql issue with defaults on text fields

--- db/migrate/20180123161959_change_long_strings_to_text.rb
+++ db/migrate/20180123161959_change_long_strings_to_text.rb
@@ -1,9 +1,10 @@
 class ChangeLongStringsToText < ActiveRecord::Migration
   def change
+    change_column_default :orgs, :links, nil
     change_column :orgs, :links, :text
+    change_column_default :templates, :links, nil
     change_column :templates, :links, :text
     change_column :identifier_schemes, :logo_url, :text
     change_column :identifier_schemes, :user_landing_url, :text
   end
 end
-
