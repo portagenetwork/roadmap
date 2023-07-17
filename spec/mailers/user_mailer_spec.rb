e6303b64e77f1518fb628f5edc998d561683f0e7
yashu <ydk31137@gmail.com>
2023-07-17 13:07:49 -0400

    Resgistration email lang test case

---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,47 @@
+#
+# frozen_string_literal: true
+
+# spec/mailers/user_mailer_spec.rb
+require 'rails_helper'
+
+RSpec.describe UserMailer, type: :mailer do
+  describe 'my_email' do
+    let(:user) { User.create(email: Faker::Internet.email) }
+
+    context 'when the user language is set to English' do
+      before do
+        @original_locale = I18n.locale
+        I18n.locale = :'en-CA'
+      end
+
+      after do
+        I18n.locale = @original_locale
+      end
+
+      let(:mail) { UserMailer.welcome_notification(user) }
+
+      it 'renders the email subject in English' do
+        expect(mail.subject).to include('Welcome to')
+        expect(mail.subject).not_to include('Bienvenue sur ')
+      end
+    end
+
+    context 'when the user language is set to French' do
+      before do
+        @original_locale = I18n.locale
+        I18n.locale = :'fr-CA'
+      end
+
+      after do
+        I18n.locale = @original_locale
+      end
+
+      let(:mail) { UserMailer.welcome_notification(user) }
+
+      it 'renders the email subject in French' do
+        expect(mail.subject).to include('Bienvenue sur ')
+        expect(mail.subject).not_to include('Welcome to ')
+      end
+    end
+  end
+end
