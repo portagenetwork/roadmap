509ea6f9713be7358d4e257e2d688cfb802f5371
aaronskiba <askiba@ualberta.ca>
2023-10-06 12:36:23 -0600

    Remove duplicate code added after v4.0.0 merge
    These duplicate lines of code resulted from merging of upstream release v4.0.0 (commit 348bf30841c4eb97b830b6c242f7c1ae3cbe0c9c). (These did not appear as merge conflicts at the time.)

--- app/controllers/plan_exports_controller.rb
+++ app/controllers/plan_exports_controller.rb
@@ -56,13 +56,6 @@ class PlanExportsController < ApplicationController
     @hash[:pa] =  Contributor.where(plan_id: @plan.id).project_administration
     @hash[:other] = Contributor.where(plan_id: @plan.id).other
 
-    # Added contributors to coverage of plans.
-    # Users will see both roles and contributor names if the role is filled
-    @hash[:data_curation] = Contributor.where(plan_id: @plan.id).data_curation
-    @hash[:investigation] = Contributor.where(plan_id: @plan.id).investigation
-    @hash[:pa] = Contributor.where(plan_id: @plan.id).project_administration
-    @hash[:other] = Contributor.where(plan_id: @plan.id).other
-
     respond_to do |format|
       format.html { show_html }
       format.csv  { show_csv }
