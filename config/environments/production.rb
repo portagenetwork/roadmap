cf811d1b732759ca48a536f27fc7df9708726622
aaronskiba <askiba@ualberta.ca>
2023-10-25 10:15:52 -0600

    Set up dmproadmap_host secret

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -126,7 +126,7 @@ Rails.application.configure do
   # This allows us to define the hostname and add it to the whitelist. If you attempt
   # to access the site and receive a 'Blocked host' error then you will need to
   # set this environment variable
-  config.hosts << ENV['DMPROADMAP_HOST'] if ENV['DMPROADMAP_HOST'].present?
+  config.hosts << Rails.application.secrets.dmproadmap_host if Rails.application.secrets.dmproadmap_host.present?
 end
 # Used by Rails' routes url_helpers (typically when including a link in an email)
-Rails.application.routes.default_url_options[:host] = ENV.fetch('DMPROADMAP_HOST', 'example.org')
+Rails.application.routes.default_url_options[:host] = Rails.application.secrets.dmproadmap_host
