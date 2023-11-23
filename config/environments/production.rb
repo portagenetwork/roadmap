99c744531fec0823335a5debec32496441273a0a
aaronskiba <askiba@ualberta.ca>
2023-11-23 14:35:34 -0700

    remove unused 'require' statement + commented code
    'require' statement is for the `config.active_record.database_selector = { delay: 2.seconds }` statement, which we are not using.
    Commented lines 102-122 are for database switching, which we are also not using.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'syslog/logger'
-require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's
@@ -98,27 +97,6 @@ Rails.application.configure do
     authentication: Rails.application.secrets.smtp_authentication || 'plain',
     enable_starttls_auto: true
   }
-
-  # Inserts middleware to perform automatic connection switching.
-  # The `database_selector` hash is used to pass options to the DatabaseSelector
-  # middleware. The `delay` is used to determine how long to wait after a write
-  # to send a subsequent read to the primary.
-  #
-  # The `database_resolver` class is used by the middleware to determine which
-  # database is appropriate to use based on the time delay.
-  #
-  # The `database_resolver_context` class is used by the middleware to set
-  # timestamps for the last write to the primary. The resolver uses the context
-  # class timestamps to determine how long to wait before reading from the
-  # replica.
-  #
-  # By default Rails will store a last write timestamp in the session. The
-  # DatabaseSelector middleware is designed as such you can define your own
-  # strategy for connection switching and pass that into the middleware through
-  # these configuration options.
-  # config.active_record.database_selector = { delay: 2.seconds }
-  # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
-  # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Rails 6+ adds middleware to prevent DNS rebinding attacks:
   #    https://guides.rubyonrails.org/configuring.html#actiondispatch-hostauthorization
