ee389946bd15338dff86ba9dd5b3e95a2c8c0cf8
aaronskiba <askiba@ualberta.ca>
2024-08-01 10:27:49 -0600

    Set active_record_store as `config.session_store`

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_dmp_roadmap_session",
-                                                      same_site: :lax
+Rails.application.config.session_store :active_record_store, key: "_dmp_roadmap_session"
