39eb671e9c32a22efb3afbc02fa198c8387c8fc5
yashu <ydk31137@gmail.com>
2024-08-01 03:30:58 -0600

    Altering the MAX cookies size for the browser

---
+++ config/initializers/cookie_size.rb
@@ -0,0 +1,8 @@
+
+# config/initializers/cookie_size.rb
+module ActionDispatch
+    class Cookies
+      # Increase the MAX_COOKIE_SIZE to 8KB (8192 bytes)
+      MAX_COOKIE_SIZE = 4600
+    end
+  end
