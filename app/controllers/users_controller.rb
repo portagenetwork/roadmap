fbe11629cd9c950b987546ad3ee98ec460a1095d
aaronskiba <askiba@ualberta.ca>
2024-08-19 11:19:26 -0600

    Make rubocop happy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -85,9 +85,7 @@ class UsersController < ApplicationController
         end
       elsif perms.include? perm
         @user.perms << perm
-        if perm.id == Perm.use_api.id
-          @user.keep_or_generate_token!
-        end
+        @user.keep_or_generate_token! if perm.id == Perm.use_api.id
         privileges_changed = true
       end
     end
