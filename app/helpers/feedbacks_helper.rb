04e082de77d56e2435fcac77ab985894d7c152f5
momo <momo.albachachi@gmail.com>
2025-06-03 13:40:36 -0600

    Added new default feedback message

--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -7,11 +7,11 @@ module FeedbacksHelper
   end
 
   def feedback_confirmation_default_message
-    _('<p>Hello %{user_name}.</p>' \
-      "<p>Your plan \"%{plan_name}\" has been submitted for feedback from an
-      administrator at your organisation. " \
-      "If you have questions pertaining to this action, please contact us
-      at %{organisation_email}.</p>")
+    # This is the default feedback message
+    _('<p>Hello %{user_name},</p>' \
+      'A member of your organisation will respond to your request to review your data management plan within 48 hours. ' \
+      'If you have questions pertaining to this action please contact your local team at %{organisation_email}, ' \
+      'or for assistance with DMP Assistant contact dmp-assistant@tech.alliancecan.ca.')
   end
 
   def feedback_constant_to_text(text, user, plan, org)
