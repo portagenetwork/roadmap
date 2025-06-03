1dffc38000862a1d1a81cada71116b7675568a44
momo <momo.albachachi@gmail.com>
2025-06-03 13:45:09 -0600

    made rubocop happy

--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -9,7 +9,8 @@ module FeedbacksHelper
   def feedback_confirmation_default_message
     # This is the default feedback message
     _('<p>Hello %{user_name},</p>' \
-      'A member of your organisation will respond to your request to review your data management plan within 48 hours. ' \
+      'A member of your organisation will respond to your request to review your data management plan
+       within 48 hours. ' \
       'If you have questions pertaining to this action please contact your local team at %{organisation_email}, ' \
       'or for assistance with DMP Assistant contact dmp-assistant@tech.alliancecan.ca.')
   end
