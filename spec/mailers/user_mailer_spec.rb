41cd5dbaeecbc724b34cc126aa59f37aee3afe4c
aaronskiba <askiba@ualberta.ca>
2025-08-01 09:22:30 -0600

    Move invitation email tests to separate _spec file

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,4 +1,3 @@
-#
 # frozen_string_literal: true
 
 # spec/mailers/user_mailer_spec.rb
@@ -63,31 +62,6 @@ RSpec.describe UserMailer, type: :mailer do
         Rails.configuration.x.application.name = 'Bar'
         expect(mail.subject).to include('Un plan de gestion des données dans Bar a été partagé avec vous')
       end
-    end
-  end
-
-  describe 'Invitation Email to User Not in DB' do
-    let(:inviter) { create(:user) }
-    let(:invitee) { User.new(email: 'newuser@example.com') }
-
-    before do
-      ActionMailer::Base.deliveries.clear
-    end
-
-    it 'sends an invitation email with a bilingual subject' do
-      # Simulate Devise invitation context
-      invitee.invitation_token = Devise.friendly_token
-      invitee.invited_by = inviter
-      invitee.invited_by_type = 'User'
-
-      # Trigger mailer directly
-      # deliver_invitation overrides devise_invitable
-      invitee.deliver_invitation
-
-      mail = ActionMailer::Base.deliveries.last
-      expect(mail.to).to include('newuser@example.com')
-      expect(mail.subject).to include('A Data Management Plan in DMP Assistant has been shared with you')
-      expect(mail.subject).to include("Un plan de gestion des données dans l'Assistant PGD a été partagé avec vous")
     end
   end
 end
