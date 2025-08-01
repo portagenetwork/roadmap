99c3e6c75d08a3e2692c824d5ab2dfad329abeb8
aaronskiba <askiba@ualberta.ca>
2025-08-01 10:14:44 -0600

    Update user_invitation_mailer_spec.rb
    These changes are intended to do the following:
    - Simplify the logic for mocking the delivery of the invitation email
    - Add the `invite_new_user` method to more closely emulate the `User.invite!` behaviour within `RolesController#create`
    - Add a test to verify that the email body is bilingual

--- spec/mailers/user_invitation_mailer_spec.rb
+++ spec/mailers/user_invitation_mailer_spec.rb
@@ -6,26 +6,46 @@ require 'rails_helper'
 RSpec.describe 'User invitation email', type: :mailer do
   describe 'Invitation Email to User Not in DB' do
     let(:inviter) { create(:user) }
-    let(:invitee) { User.new(email: 'newuser@example.com') }
+    let(:invitee_email) { 'newuser@example.com' }
+    # For ensuring I18n.locale value remains intact.
+    let(:original_locale) { I18n.locale }
 
     before do
       ActionMailer::Base.deliveries.clear
+      # TODO: Update spec/support/locales.rb to actually use DMP Assistant's locales.
+      @original_locales = I18n.available_locales
+      I18n.available_locales = %i[en-CA fr-CA]
     end
 
-    it 'sends an invitation email with a bilingual subject' do
-      # Simulate Devise invitation context
-      invitee.invitation_token = Devise.friendly_token
-      invitee.invited_by = inviter
-      invitee.invited_by_type = 'User'
+    after do
+      I18n.available_locales = @original_locales
+    end
 
-      # Trigger mailer directly
-      # deliver_invitation overrides devise_invitable
-      invitee.deliver_invitation
-
+    it 'sends an invitation with the expected content' do
+      invite_new_user
       mail = ActionMailer::Base.deliveries.last
-      expect(mail.to).to include('newuser@example.com')
-      expect(mail.subject).to include('A Data Management Plan in DMP Assistant has been shared with you')
-      expect(mail.subject).to include("Un plan de gestion des données dans l'Assistant PGD a été partagé avec vous")
+      expect(mail.to).to include(invitee_email)
+      # The subject line is bilingual
+      expect(mail.subject).to eq('A Data Management Plan in DMP Assistant has been shared with you / ' \
+                                 "Un plan de gestion des données dans l'Assistant PGD a été partagé avec vous")
+      # The body is bilingual
+      # (TODO: Fix this generic name greeting)
+      expect(mail.body).to include('Hello First Name Surname')
+      expect(mail.body).to include('Bonjour First Name Surname')
+      # I18n.locale value remains intact
+      expect(I18n.locale).to eq(original_locale)
     end
+  end
+
+  private
+
+  def invite_new_user
+    # This code is copied from RolesController#create to simulate its User.invite! behavior.
+    # The controller action is large, so copying the code here is a pragmatic testing approach
+    User.invite!({ email: invitee_email,
+                   firstname: _('First Name'),
+                   surname: _('Surname'),
+                   org: inviter.org },
+                 inviter)
   end
 end
