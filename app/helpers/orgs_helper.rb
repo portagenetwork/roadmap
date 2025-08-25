247a0f85d1366a0f7cdf23b9b69f5d2f00a3a80e
momo3404 <momo.albachachi@gmail.com>
2025-08-25 13:43:36 -0600

    Correct sample message behaviour and rename functions

--- app/helpers/orgs_helper.rb
+++ app/helpers/orgs_helper.rb
@@ -5,19 +5,20 @@ module OrgsHelper
   EMAIL_PLACEHOLDER = '[Organisation Contact Email Placeholder]'
   PLAN_PLACEHOLDER = '[Plan Name Placeholder]'
   USER_PLACEHOLDER = ''
-  # Displays a feedback message
+
+  # Displays a feedback message that the user can edit in the org/Request Feedback section
   #
   # org - The current Org who owns the feedback message being displayed
   # current_user - The current user we're showing feedback message to
   # feedack_message - The feedback message we're displaying
-  # plan_name - Name of the plan we're displaying the feedback message for;
-  #   set to nil because there is no plan name in org feedback form
+  # plan_name - Name of the plan we're displaying the feedback message for
   #
   # Returns String
-  def display_feedback_message(org, current_user, feedback_message, plan_name = nil)
+  def display_editable_feedback_message(org, current_user, feedback_message, plan_name)
     email = org.contact_email || EMAIL_PLACEHOLDER
     username = current_user.name(false) || USER_PLACEHOLDER
     plan_title = plan_name || PLAN_PLACEHOLDER
+
     format(
       _(feedback_message),
       user_name: username, organisation_email: email, plan_name: plan_title
@@ -31,6 +32,25 @@ module OrgsHelper
       message = _('Unable to render feedback message: ')
       message + _(e.message.to_s)
     end
+  end
+
+  # Displays a sample feedback message in the org/Request Feedback section
+  # that is meant to serve as an example
+  #
+  # org - The current Org who owns the feedback message being displayed
+  # current_user - The current user we're showing feedback message to
+  #
+  # Returns String
+  def display_sample_feedback_message(org, current_user)
+    email = org.contact_email || EMAIL_PLACEHOLDER
+    username = current_user.name(false) || USER_PLACEHOLDER
+
+    format(_('<p>Hello %{user_name},</p>' \
+      'A member of your organisation will respond to your request to review your data management plan
+       within 48 hours. ' \
+      'If you have questions pertaining to this action please contact your local team at %{organisation_email}, ' \
+      'or for assistance with DMP Assistant contact dmp-assistant@tech.alliancecan.ca.'), user_name: username,
+                                                                                          organisation_email: email)
   end
 
   # The preferred logo url for the current configuration. If DRAGONFLY_AWS is true, return
