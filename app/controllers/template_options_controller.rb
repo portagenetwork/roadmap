40c02999019fbe507cbfaa19684401b234e4725f
aaronskiba <askiba@ualberta.ca>
2025-10-08 11:38:42 -0600

    Refactor `sort_templates()`

--- app/controllers/template_options_controller.rb
+++ app/controllers/template_options_controller.rb
@@ -69,47 +69,39 @@ class TemplateOptionsController < ApplicationController
     %i[id name url language abbreviation ror fundref weight score]
   end
 
-  def alliance_templates(templates) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity
-    # The default funder is the Alliance
-    default_funder_id = Rails.application.config.default_funder_id
+  # Orders templates for dropdown:
+  # 1. Org templates
+  # 2. Priority funder templates (Simplified, Default)
+  # 3. Remaining funder templates
+  def sort_templates(templates, org)
+    # Get the funder templates with the Priority templates ordered in front
+    sorted_funder_templates = sort_funder_templates(templates)
 
-    simplified = templates.find do |t|
-      t.title.start_with?('Alliance Simplified Template') &&
-        t.org&.id == default_funder_id
+    # If an org was selected and the selected org is not the default funder
+    if org&.id && org.id != Rails.application.config.default_funder_id
+      # Return the templates with the org templates ordered in front
+      org_templates = templates.select { |t| t.org_id == org.id }
+      org_templates + sorted_funder_templates
+    # else, either no org or default_funder was selected
+    else
+      # All of the templates are funder templates
+      sorted_funder_templates
     end
-
-    alliance = templates.find do |t|
-      t.title == 'Alliance Template' &&
-        t.org&.id == default_funder_id &&
-        t.is_default == true
-    end
-
-    [simplified, alliance].compact
   end
 
-  # Assign the order of templates in the dropdown menu
-  # The desired order of templates in the dropdown is:
-  # All organizational templates first followed by the Alliance Simplified Template
-  # Followed by the Alliance Template and then all remaining templates
-  def sort_templates(templates, org) # rubocop:disable Metrics/AbcSize
-    # Get all templates belonging to the selected organization
-    org_templates = templates.select { |template| template.org_id == org&.id }
-    # Get the Alliance Simplified and Full Templates
-    alliance_templates = alliance_templates(templates)
+  # Orders funder templates:
+  # 1. Priority (Simplified, Default)
+  # 2. Remaining funder templates
+  def sort_funder_templates(templates)
+    funder_templates = templates.select { |t| t.org_id == Rails.application.config.default_funder_id }
 
-    org_is_alliance = org&.id == Rails.application.config.default_funder_id
+    # Identify priority templates
+    # NOTE: This will have to be updated if the `simplified` template is ever renamed
+    simplified = funder_templates.find { |t| t.title.start_with?('Alliance Simplified Template') }
+    default   = funder_templates.find(&:is_default)
+    priority  = [simplified, default].compact
 
-    # If the chosen org is the Alliance, its templates must not be removed
-    remaining_templates = if org_is_alliance
-                            templates - alliance_templates
-                          else
-                            templates - org_templates - alliance_templates
-                          end
-
-    if org_is_alliance
-      (alliance_templates + remaining_templates).uniq
-    else
-      (org_templates + alliance_templates + remaining_templates).uniq
-    end
+    # Return priority first, then remaining funder templates
+    priority + (funder_templates - priority)
   end
 end
