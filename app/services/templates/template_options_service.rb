fc9bcbabce43191fdba4eaaa0759378625923928
aaronskiba <askiba@ualberta.ca>
2025-11-13 14:14:50 -0700

    Use local variable and always return array in TemplateOptionsService.available_templates
    - Replace the class instance variable @templates with a local templates variable to avoid shared state.
    - Update top level return to return an array rather than nil
      - `TemplateOptionsController#index` and `PlansController#find_valid_template` each expect an array.

--- app/services/templates/template_options_service.rb
+++ app/services/templates/template_options_service.rb
@@ -8,16 +8,16 @@ module Templates
     def self.available_templates(org)
       funder = Org.find(Rails.application.config.default_funder_id)
 
-      @templates = []
+      templates = []
 
-      return unless (org.present? && !org.new_record?) || (funder.present? && !funder.new_record?)
+      return templates unless (org.present? && !org.new_record?) || (funder.present? && !funder.new_record?)
 
       if org.present? && !org.new_record?
         # Load the funder's template(s)
         # (`Template.default` belongs to `funder` and will be fetched as part of this query)
-        @templates = Template.latest_customizable.where(org_id: funder.id).sort_by(&:title).to_a
+        templates = Template.latest_customizable.where(org_id: funder.id).sort_by(&:title).to_a
         # Wherever possible, replace funder templates with organisational customizations
-        @templates = @templates.map do |tmplt|
+        templates = templates.map do |tmplt|
           customization = Template.published
                                   .latest_customized_version(tmplt.family_id,
                                                              org.id).first
@@ -31,16 +31,16 @@ module Templates
         # We are using a default funder to provide with the default templates, but
         # We still want to provide the organization templates.
         # Retrieve the Org's templates
-        @templates << Template.published.organisationally_visible.where(org_id: org.id,
-                                                                        customization_of: nil).sort_by(&:title).to_a
+        templates << Template.published.organisationally_visible.where(org_id: org.id,
+                                                                       customization_of: nil).sort_by(&:title).to_a
       else
         # if'No Primary Research Institution' checkbox is checked,
         # only show publicly available template without customization
         # (`Template.default` belongs to `funder` and will be fetched as part of this query)
-        @templates << Template.published.publicly_visible.where(org_id: funder.id,
-                                                                customization_of: nil).sort_by(&:title).to_a
+        templates << Template.published.publicly_visible.where(org_id: funder.id,
+                                                               customization_of: nil).sort_by(&:title).to_a
       end
-      @templates = @templates.flatten
+      templates.flatten
     end
     # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
     # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
