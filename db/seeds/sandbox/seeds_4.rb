f5ec2b0e5f42b180fefe9e28f8c54b2294acbba4
aaronskiba <askiba@ualberta.ca>
2026-01-13 14:15:02 -0700

    Refactor `seeds_4.rb`
    - Added variables for `default_org`, `english`, and `french`, which addresses previous repeated db calls.
    - Refactored the creation of regular users.
      - Previously, we had 3 separate (1..20).each to address the (org, language) groupings for users.
      - Now we create the same users and (org, language) groupings via a single loop
    - Removed Faker references
      - Although, this seeds file will be used to populate the sandbox db, the "sandbox" environment will still use `RAILS_ENV=production`. Thus, Faker won't be available.
      - Now we have firstname = "tester#{index}" (unique value for each user), surname: 'Test' (same for every user), and `api_token: Faker::Lorem.word` has been removed.

--- db/seeds/sandbox/seeds_4.rb
+++ db/seeds/sandbox/seeds_4.rb
@@ -1,108 +1,84 @@
+# frozen_string_literal: true
+
 # One super admin for the default org
-  # One funder Admin for the funder organization and an Org admin and User for the institutional organization
-  # -------------------------------------------------------
-  # Admins are created 5 years ago
-  Faker::Config.random = Random.new(60)
-  pwd = Rails.application.secrets.user_password.to_s # pwd for regular user
-  english_test_org = Org.find_by(abbreviation: 'IEO')
-  french_test_org = Org.find_by(abbreviation: 'OEO')
-  users = [
-    {email: "dmp.super.admin@engagedri.ca",
-     firstname: "Super",
-     surname: "Admin",
-     language_id: 1,
-     password: pwd,
-     password_confirmation: pwd,
-     org: Org.find_by(id: Rails.application.config.default_funder_id),
-     language: Language.all.first,
-     perms: Perm.all,
-     accept_terms: true,
-     api_token: Org.column_defaults['api_token'],
-     confirmed_at: 5.years.ago,
-     created_at: 5.years.ago,
-    active:1},
-    {email: "dmp.test.user.admin@engagedri.ca",
-     firstname: "Test",
-     surname: "User",
-     password: pwd,
-     password_confirmation: pwd,
-     org: english_test_org,
-     language_id: 1, # English
-     perms: Perm.where.not(name: ['admin', 'add_organisations', 'change_org_affiliation', 'grant_api_to_orgs']),
-     accept_terms: true,
-     api_token: Org.column_defaults['api_token'],
-     confirmed_at: 5.years.ago,
-     created_at: 5.years.ago,
-     active:1
-    },
-    {email: "dmp.utilisateur.test.admin@engagedri.ca",
-      firstname: "Utilisateur",
-      surname: "test",
+# One funder Admin for the funder organization and an Org admin and User for the institutional organization
+# -------------------------------------------------------
+# Admins are created 5 years ago
+pwd = Rails.application.secrets.user_password.to_s # pwd for regular user
+default_org = Org.find(Rails.application.config.default_funder_id)
+english_test_org = Org.find_by!(abbreviation: 'IEO')
+french_test_org = Org.find_by!(abbreviation: 'OEO')
+english = Language.find_by!(abbreviation: 'en-CA')
+french = Language.find_by!(abbreviation: 'fr-CA')
+
+admin_users = [
+  { email: 'dmp.super.admin@engagedri.ca',
+    firstname: 'Super',
+    surname: 'Admin',
+    password: pwd,
+    password_confirmation: pwd,
+    org: default_org,
+    language: english,
+    perms: Perm.all,
+    accept_terms: true,
+    api_token: Org.column_defaults['api_token'],
+    confirmed_at: 5.years.ago,
+    created_at: 5.years.ago,
+    active: 1 },
+  { email: 'dmp.test.user.admin@engagedri.ca',
+    firstname: 'Test',
+    surname: 'User',
+    password: pwd,
+    password_confirmation: pwd,
+    org: english_test_org,
+    language: english,
+    perms: Perm.where.not(name: %w[admin add_organisations change_org_affiliation grant_api_to_orgs]),
+    accept_terms: true,
+    api_token: Org.column_defaults['api_token'],
+    confirmed_at: 5.years.ago,
+    created_at: 5.years.ago,
+    active: 1 },
+  { email: 'dmp.utilisateur.test.admin@engagedri.ca',
+    firstname: 'Utilisateur',
+    surname: 'test',
+    password: pwd,
+    password_confirmation: pwd,
+    language: french,
+    org: french_test_org,
+    perms: Perm.where.not(name: %w[admin add_organisations change_org_affiliation grant_api_to_orgs]),
+    accept_terms: true,
+    api_token: Org.column_defaults['api_token'],
+    confirmed_at: 5.years.ago,
+    created_at: 5.years.ago,
+    active: 1 }
+]
+admin_users.each { |u| User.create(u) }
+
+# Some existing users for statistics. Creation times are within 12 months
+user_groups = [{ org: default_org,      language: english },
+               { org: english_test_org, language: english },
+               { org: french_test_org,  language: french }]
+
+# Create 60 users (20 for each user_group)
+user_groups.each_with_index do |group, group_index|
+  (1..20).each do |i|
+    # index is unique for each user (1 thru 60)
+    index = i + (20 * group_index)
+    firstname = "tester#{index}"
+    user = {
+      email: "#{firstname}@test.ca",
+      firstname: firstname,
+      surname: 'Test',
       password: pwd,
       password_confirmation: pwd,
-      language_id: 2, # French
-      org: french_test_org,
-      perms: Perm.where.not(name: ['admin', 'add_organisations', 'change_org_affiliation', 'grant_api_to_orgs']),
-      accept_terms: true,
-      api_token: Org.column_defaults['api_token'],
-      confirmed_at: 5.years.ago,
-      created_at: 5.years.ago,
-      active:1
-     }
-  ]
-  users.each{ |u| User.create(u) }
-  # Some existing users for statistics. Creation times are within 12 months
-  (1..20).each do |index|
-    user = {
-        email: "tester" + index.to_s + "@test.ca",
-        firstname: Faker::Name.first_name,
-        surname: Faker::Name.last_name,
-        password: pwd,
-        password_confirmation: pwd,
-        org: Org.find_by(id: Rails.application.config.default_funder_id),
-        language: Language.all.first,
-        perms: [],
-        accept_terms: true,
-        api_token: Faker::Lorem.word,
-        confirmed_at: rand(1...12).month.ago,
-        created_at: rand(1...12).month.ago,
-        active:1
-    }
-    User.create!(user)
-  end
-  (1..20).each do |index|
-    user = {
-        email: "tester" + (index+20).to_s + "@test.ca",
-        firstname: Faker::Name.first_name,
-        surname: Faker::Name.last_name,
-        password: pwd,
-        password_confirmation: pwd,
-        org: english_test_org,
-        language: Language.all.first,
-        perms: [],
-        accept_terms: true,
-        api_token: Faker::Lorem.word,
-        confirmed_at: rand(1...12).month.ago,
-        created_at: rand(1...12).month.ago,
-        active:1
-    }
-    User.create!(user)
-  end
-  (1..20).each do |index|
-    user = {
-      email: "tester" + (index+40).to_s + "@test.ca",
-      firstname: Faker::Name.first_name,
-      surname: Faker::Name.last_name,
-      password: pwd,
-      password_confirmation: pwd,
-      org: french_test_org,
-      language: Language.all.last, # French
+      org: group[:org],
+      language: group[:language],
       perms: [],
       accept_terms: true,
-      api_token: Faker::Lorem.word,
       confirmed_at: rand(1...12).month.ago,
       created_at: rand(1...12).month.ago,
-      active:1
+      active: 1
     }
     User.create!(user)
   end
+end
