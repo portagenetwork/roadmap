4640b50f3b5c3776f21be0b8faa3336ed949c202
momo3404 <momo.albachachi@gmail.com>
2026-02-06 11:37:21 -0700

    Add complete plan flag to `plans_controller`
    - Edit plan query to check for user role to prevent eager loading
    - Add set_complete_param function that sets complete flag according to its value if its set to true in the API call

--- app/controllers/api/v2/plans_controller.rb
+++ app/controllers/api/v2/plans_controller.rb
@@ -4,12 +4,13 @@ module Api
   module V2
     class PlansController < BaseApiController # rubocop:todo Style/Documentation
       respond_to :json
+      before_action :set_complete_param, only: %i[show index]
 
       # GET /api/v2/plans/:id
       def show
         raise Pundit::NotAuthorizedError unless @scopes.include?('read')
 
-        @plan = Plan.find_by(id: params[:id])
+        @plan = Plan.includes(roles: :user).find_by(id: params[:id])
 
         raise Pundit::NotAuthorizedError unless @plan.present?
 
@@ -27,6 +28,13 @@ module Api
         @plans = PlansPolicy::Scope.new(@resource_owner).resolve
         @items = paginate_response(results: @plans)
         render '/api/v2/plans/index', status: :ok
+      end
+
+      private
+
+      # GET /api/v2/plans?complete=true and  /api/v2/plans/:id?complete=true
+      def set_complete_param
+        @complete = params[:complete].to_s.downcase == 'true'
       end
     end
   end
