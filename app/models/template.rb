2a9831f7fb4dae4a5a772c0e5325aa2122c21b7a
aaronskiba <askiba@ualberta.ca>
2026-02-24 13:16:33 -0700

    Eager load identifier_scheme in Template.for_api_v2
    Addresses the following Bullet warning:
    
    user: aaron
    GET /api/v2/templates
    USE eager loading detected
      Identifier => [:identifier_scheme]
      Add to your query: .includes([:identifier_scheme])
    Call stack
      /home/aaron/Documents/GitHub/roadmap/app/presenters/api/v2/org_presenter.rb:9:in `block in affiliation_id'
      /home/aaron/Documents/GitHub/roadmap/app/presenters/api/v2/org_presenter.rb:9:in `affiliation_id'
      /home/aaron/Documents/GitHub/roadmap/app/views/api/v2/orgs/_show.json.jbuilder:11:in `block in _app_views_api_v__orgs__show_json_jbuilder__1237609272914133563_48080'
      /home/aaron/Documents/GitHub/roadmap/app/views/api/v2/orgs/_show.json.jbuilder:10:in `_app_views_api_v__orgs__show_json_jbuilder__1237609272914133563_48080'
      /home/aaron/Documents/GitHub/roadmap/app/views/api/v2/templates/index.json.jbuilder:16:in `block (3 levels) in _app_views_api_v__templates_index_json_jbuilder___4313931085157922975_47640'
      /home/aaron/Documents/GitHub/roadmap/app/views/api/v2/templates/index.json.jbuilder:15:in `block (2 levels) in _app_views_api_v__templates_index_json_jbuilder___4313931085157922975_47640'
      /home/aaron/Documents/GitHub/roadmap/app/views/api/v2/templates/index.json.jbuilder:8:in `block in _app_views_api_v__templates_index_json_jbuilder___4313931085157922975_47640'
      /home/aaron/Documents/GitHub/roadmap/app/views/api/v2/templates/index.json.jbuilder:5:in `_app_views_api_v__templates_index_json_jbuilder___4313931085157922975_47640'
      /home/aaron/Documents/GitHub/roadmap/app/controllers/api/v2/templates_controller.rb:13:in `index'

--- app/models/template.rb
+++ app/models/template.rb
@@ -218,7 +218,7 @@ class Template < ApplicationRecord
   scope :for_api_v2, lambda { |org_id|
     org_templates = organisationally_visible.where(org_id: org_id)
     public_templates = publicly_visible.where(customization_of: nil)
-    includes(org: :identifiers)
+    includes(org: { identifiers: :identifier_scheme })
       .joins(:org)
       .published
       .merge(org_templates.or(public_templates))
