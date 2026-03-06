892f2986fd23ff6319a9a71d9a870a4eb8d7dd45
momo3404 <85097704+momo3404@users.noreply.github.com>
2026-03-06 13:08:35 -0700

    Remove unused paths from `application_helper`
    - These paths were removed when the content for the pages they were in was updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -53,24 +53,6 @@ module ApplicationHelper
     end
   end
 
-  def how_to_manage_your_data_path
-    if I18n.locale == :'fr-CA'
-      'https://portagenetwork.ca/fr/outils-et-ressources/assistant-pgd/comment-gerer-vos-donnees/'
-    else
-      # Handling :'en-CA' locale
-      'https://portagenetwork.ca/tools-and-resources/dmp-assistant/how-to-manage-your-data/'
-    end
-  end
-
-  def contacts_at_your_instutution_path
-    if I18n.locale == :'fr-CA'
-      'https://alliancecan.ca/fr/services/gestion-des-donnees-de-recherche/apprentissage-et-ressources/personnes-ressources-dans-les-etablissements'
-    else
-      # Handling :'en-CA' locale
-      'https://alliancecan.ca/en/services/research-data-management/learning-and-training/institutional-contacts'
-    end
-  end
-
   def training_resources_path
     if I18n.locale == :'fr-CA'
       'https://alliancecan.ca/fr/services/gestion-des-donnees-de-recherche/apprentissage-et-ressources/ressources-de-formation'
