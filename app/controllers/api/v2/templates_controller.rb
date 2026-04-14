6d01c485464db0ee117d8a08498c77d217a4d5a8
momo3404 <85097704+momo3404@users.noreply.github.com>
2026-04-14 15:02:27 -0600

    Add `GET /api/v2/templates/:id` endpoint to `templates_controller`

--- app/controllers/api/v2/templates_controller.rb
+++ app/controllers/api/v2/templates_controller.rb
@@ -6,11 +6,27 @@ module Api
     class TemplatesController < BaseApiController
       respond_to :json
 
+      # GET /api/v2/templates/:id
+      def show
+        template = templates_scope.find_by(id: params[:id])
+
+        return render_error(errors: ['Template not found'], status: :not_found) unless template
+
+        @items = [template]
+        render '/api/v2/templates/index', status: :ok
+      end
+
       # GET /api/v2/templates
       def index
         templates = Api::V2::TemplatesPolicy::Scope.new(@resource_owner).resolve
         @items = paginate_response(results: templates)
         render '/api/v2/templates/index', status: :ok
+      end
+
+      private
+
+      def templates_scope
+        Api::V2::TemplatesPolicy::Scope.new(@resource_owner).resolve
       end
     end
   end
