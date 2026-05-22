1fe79b5371231d5e71f1b4482645b2ec9f135061
momo3404 <85097704+momo3404@users.noreply.github.com>
2026-05-22 13:22:13 -0600

    Update `routes.rb` to include `fetch_doi`

--- config/routes.rb
+++ config/routes.rb
@@ -131,7 +131,11 @@ Rails.application.routes.draw do
 
     resources :contributors, except: %i[show]
 
-    resources :research_outputs, except: %i[show]
+    resources :research_outputs, except: %i[show] do
+      collection do
+        get :fetch_doi
+      end
+    end
 
     member do
       get 'answer'
