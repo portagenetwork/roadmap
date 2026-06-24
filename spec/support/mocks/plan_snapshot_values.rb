f2e1c3cf379b9ecf69f337b0b9370384fe7c6c80
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2026-06-24 10:47:24 -0600

    feat(PlanSnapshotRdaJson): add RDA JSON parsing service object
    - Introduce `PlanSnapshotRdaJson` service to encapsulate RDA JSON parsing
    - Add memoized `rda_json_reader` to `PlanSnapshot`
    - Delegate dmp `identifier` and `identifier_type` methods from `PlanSnapshot` to reader
    - Add to and update supporting test mocks for RDA JSON structures
    - Add specs for service and delegation behavior

--- spec/support/mocks/plan_snapshot_values.rb
+++ spec/support/mocks/plan_snapshot_values.rb
@@ -6,14 +6,15 @@ module PlanSnapshotValues
     Array.new(32) { ('a'..'f').to_a.concat(('0'..'9').to_a).sample }.join
   end
 
-  def self.mock_rda_json
+  def self.mock_rda_json(type: 'url', identifier: mock_plan_identifier_url)
     {
       'dmp' => {
         'title' => 'Test Plan',
         'description' => 'A test plan description.',
         'language' => 'eng',
         'created' => '2026-05-20T00:00:00Z',
-        'modified' => '2026-05-20T00:00:00Z'
+        'modified' => '2026-05-20T00:00:00Z',
+        'dmp_id' => mock_dmp_id(type: type, identifier: identifier)
       }
     }
   end
@@ -31,5 +32,16 @@ module PlanSnapshotValues
         }
       ]
     }
+  end
+
+  def self.mock_dmp_id(type: 'url', identifier: mock_plan_identifier_url)
+    {
+      'type' => type,
+      'identifier' => identifier
+    }
+  end
+
+  def self.mock_plan_identifier_url
+    'http://example.org/api/v2/plans/18244'
   end
 end
