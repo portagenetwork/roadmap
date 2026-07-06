a232e7955d4630083f7f717caf03d23ca3f0c0c2
momo3404 <85097704+momo3404@users.noreply.github.com>
2026-07-06 14:10:04 -0600

    Add regex validation for DOIs

--- app/services/external_apis/doi_resolution_service.rb
+++ app/services/external_apis/doi_resolution_service.rb
@@ -1,16 +1,24 @@
 # frozen_string_literal: true
 
 module ExternalApis
-  # This service calls DataCite and Crossref services accordingly when parsing DOI
+  # This service calls DataCite and Crossref services accordingly when parsing DOIs
   class DoiResolutionService
     class << self
+      # Matches ~99% of modern Crossref DOIs
+      MODERN_DOI_REGEX = %r{10\.\d{4,9}/[-._;()/:A-Z0-9]+}i
+
+      # Catch-all for early DOIs with complex/opaque formatting
+      OLD_DOI_REGEX = %r{10\.1002/[^\s]+}i
+
       # Main function for the "Add Research Output by DOI" feature
       def fetch_metadata(doi:)
         return nil if doi.blank?
 
         # Strip 'https://doi.org/'
         clean_doi = doi.strip.gsub(%r{^https?://doi.org/}, '')
-        return nil if clean_doi.blank?
+        # Validate DOIs with regex
+        # See https://www.crossref.org/blog/dois-and-matching-regular-expressions/ for more
+        return nil unless doi.to_s.match?(MODERN_DOI_REGEX) || doi.to_s.match?(OLD_DOI_REGEX)
 
         # Try DataCite first
         metadata = execute_fetch(ExternalApis::DataciteService, 'datacite', clean_doi)
