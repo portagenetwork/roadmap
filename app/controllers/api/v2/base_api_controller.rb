84ba419030b835a28e73ddd3f0bcb89a8d2da3e5
momo3404 <85097704+momo3404@users.noreply.github.com>
2026-08-07 14:49:10 -0600

    Refactor `base_response_content` in `base_api_controller`
    - Move code assigning resource_owner and presence check to authorize_resource_owner from base_response_content for cleaner readability and functionality

--- app/controllers/api/v2/base_api_controller.rb
+++ app/controllers/api/v2/base_api_controller.rb
@@ -8,10 +8,10 @@ module Api
 
       # call doorkeeper to authorize the request
       before_action :doorkeeper_authorize!, except: %i[heartbeat]
+      # Authorize resource owner, check if the user account associated with the token is active
+      before_action :authorize_resource_owner, except: %i[heartbeat]
       # get details of server (e.g. DMPonline) and client app
       before_action :base_response_content
-      # check if the user account associated with the token is active
-      before_action :authorize_resource_owner, except: %i[heartbeat]
 
       before_action :log_access
 
@@ -52,20 +52,20 @@ module Api
       # define instance variable json and associated getter and setter methods
       attr_accessor :json
 
+      def authorize_resource_owner
+        return unless doorkeeper_token&.resource_owner_id.present?
+
+        @resource_owner = User.find_by(id: doorkeeper_token.resource_owner_id)
+
+        return if @resource_owner.present? && @resource_owner.active?
+
+        render_error(errors: _('User account has been deactivated.'), status: :unauthorized)
+      end
+
       def base_response_content
         @application = ApplicationService.application_name
         @client = doorkeeper_token&.application
         @caller = @client&.name || request.remote_ip
-        return unless doorkeeper_token&.resource_owner_id
-
-        @resource_owner = User.find(doorkeeper_token.resource_owner_id)
-      end
-
-      # Reject request if the resource owner account is deactivated
-      def authorize_resource_owner
-        return if @resource_owner.active?
-
-        render_error(errors: _('User account has been deactivated.'), status: :unauthorized)
       end
 
       def log_access
